073ba2a6ff6b811bccf3e5dc21c651b114f44382
Tomas <tomasbazan5@gmail.com>
2024-03-02 17:47:09 -0300

    2022 old code of advent of code

---
+++ 2022/JavaScript/first.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var readlineSync = require("readline-sync");
+function findElfWithMostCalories(calories) {
+    var caloriesForElf = calories.split('\n\n');
+    console.log(caloriesForElf);
+    return 1;
+}
+var userInput = readlineSync.question('input: ');
---
+++ 2022/JavaScript/first.ts
@@ -0,0 +1,9 @@
+import * as readlineSync from 'readline-sync';
+
+function findElfWithMostCalories(calories: string): number {
+  const caloriesForElf: string[] = calories.split('\n\n');
+  console.log(caloriesForElf);
+  return 1;
+}
+const userInput: string = readlineSync.question('input: ');
+
